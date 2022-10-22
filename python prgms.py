5d233253dd3f3f35ab39220279b3f20d84a10534
VakaHemalatha <115526322+VakaHemalatha@users.noreply.github.com>
2022-10-22 12:33:50 +0530

    Delete python prgms.py

--- python prgms.py
+++
@@ -1,7 +0,0 @@
-n=str(input('enter a month and day of birth:'))
-if n=='dec22-jan10':
-    print('capricon')
-elif n=='jan20 to feb18':
-    print('aquarius')
-elif n=='feb19 to mar20':
-    print('pisces')
